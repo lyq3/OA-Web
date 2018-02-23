b538cd5dae6c89c9927a536b244b20dc81e5220a
menglihuan <297234995@qq.com>
2018-02-24 00:05:34 +0800

    登录页面

--- page/login/login.js
+++ page/login/login.js
@@ -4,17 +4,23 @@ layui.use(['form','layer','jquery'],function(){
         $ = layui.jquery;
 
     $(".loginBody .seraph").click(function(){
-        layer.msg("这只是做个样式，至于功能，你见过哪个后台能这样登录的？还是老老实实的找管理员去注册吧",{
+        layer.msg("功能仍在开发中,敬请期待",{
             time:5000
         });
     })
 
     //登录按钮
     form.on("submit(login)",function(data){
-        $(this).text("登录中...").attr("disabled","disabled").addClass("layui-disabled");
-        setTimeout(function(){
-            window.location.href = "/layuicms2.0";
-        },1000);
+        if($('#userName').val() =='admin'&&$('#password').val() =='123321'&&$('#code').val() =='jgmxj'){
+            $(this).text("登录中...").attr("disabled","disabled").addClass("layui-disabled");
+            setTimeout(function(){
+                window.location.href = "../../index.html";
+            },1000);
+        }else{
+            layer.msg("账号或密码错误",{
+                time:5000
+            });
+        }
         return false;
     })
 
