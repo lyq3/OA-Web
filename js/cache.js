1ac8fda6c184b339022cb1381c53db3626408d48
menglihuan <297234995@qq.com>
2018-02-24 16:47:26 +0800

    修账号文字，版权等

--- js/cache.js
+++ js/cache.js
@@ -30,9 +30,9 @@ layui.use(['form','jquery',"layer"],function() {
             area: '300px',
             shade: 0.8,
             id: 'LAY_layuipro',
-            btn: ['火速围观'],
+            btn: ['确定'],
             moveType: 1,
-            content: '<div style="padding:15px 20px; text-align:justify; line-height: 22px; text-indent:2em;border-bottom:1px solid #e2e2e2;"><p class="layui-red">周末写的，仅供参考</span></p></div>',
+            content: '<div style="padding:15px 20px; text-align:justify; line-height: 22px; text-indent:2em;border-bottom:1px solid #e2e2e2;"><p class="layui-red">部分功能仍在紧张开发中,敬请期待</span></p></div>',
             success: function(layero){
                 var btn = layero.find('.layui-layer-btn');
                 btn.css('text-align', 'center');
@@ -65,12 +65,12 @@ layui.use(['form','jquery',"layer"],function() {
             type : 1,
             content : '<div class="admin-header-lock" id="lock-box">'+
                             '<div class="admin-header-lock-img"><img src="images/face.jpg" class="userAvatar"/></div>'+
-                            '<div class="admin-header-lock-name" id="lockUserName">卡卢比</div>'+
+                            '<div class="admin-header-lock-name" id="lockUserName">超级管理员</div>'+
                             '<div class="input_btn">'+
                                 '<input type="password" class="admin-header-lock-input layui-input" autocomplete="off" placeholder="请输入密码解锁.." name="lockPwd" id="lockPwd" />'+
                                 '<button class="layui-btn" id="unlock">解锁</button>'+
                             '</div>'+
-                            '<p>请输入“123456”，否则不会解锁成功哦！！！</p>'+
+                            // '<p>请输入“123456”，否则不会解锁成功哦！！！</p>'+
                         '</div>',
             closeBtn : 0,
             shade : 0.9,
